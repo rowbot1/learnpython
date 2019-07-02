859a4e718dfc20d9c9821d7fc47e5510b04b4bcd
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-03 00:10:03 +0100

    Update Odd_or_Even.py

--- Odd_or_Even.py
+++ Odd_or_Even.py
@@ -1,3 +1,11 @@
+"""Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+If the number is a multiple of 4, print out a different message.
+Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message"""
+
+
 numb = (int(input("Enter a number to reveal if it is even or odd and if it's a multiple of 4: ")))
 #  If you divide the input num by 2 and there remainder (mod - %) is 0 print even - else print odd
 if numb % 2 == 0:
