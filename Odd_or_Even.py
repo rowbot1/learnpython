0a6f8c6e557416cea8a669878116c8bc69eed00b
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-03 08:54:27 +0100

    Update Odd_or_Even.py

--- Odd_or_Even.py
+++ Odd_or_Even.py
@@ -5,22 +5,21 @@ Extras:
 If the number is a multiple of 4, print out a different message.
 Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message"""
 
-
 numb = (int(input("Enter a number to reveal if it is even or odd and if it's a multiple of 4: ")))
 #  If you divide the input num by 2 and there remainder (mod - %) is 0 print even - else print odd
 if numb % 2 == 0:
-    print("even")
+    print("Even")
 else:
-    print ("odd")
+    print ("Odd")
 #checks if number is a multiple of 4
 if numb % 4 == 0:
     print("and is a multiple of 4")
 
 print("-----------Check to see if a number divides into another number equally-------------")
 
-num = (int(input("Enter a number: ")))
-check = (int(input("Enter a number: " )))
-
+num = (int(input("Enter First Number: ")))
+check = (int(input("Enter 2nd Number: " )))
+121
 if num % check == 0:
     print (check, "Divides evenly into" ,num)
 else:
