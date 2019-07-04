dcb0261fc7c4122ceb40164923ac8c285fc19ece
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-04 20:16:00 +0100

    Update List_OverLap2.py
    to be updated

--- List_OverLap2.py
+++ List_OverLap2.py
@@ -1,14 +1,16 @@
 import random
-numbers_1 = (int(input("List 1 size: ")))
-numbers_2 = (int(input("List 2 size: ")))
+numbers_1 = int(input("List 1 size: "))
+numbers_2 = int(input("List 2 size: "))
 
-
+#take user input and generate a range
 for n1 in range(numbers_1):
-    x = (random.randint(0 , n1))
-    print(x)
+    x = random.randint(0 , n1)
+    
     for n2 in range(numbers_2):
-            y = (random.randint(0, n2))
-            c = x + y
+        y = random.randint(0, n2)
+        c = x + y
 
-unique_list = set(c)
-print(unique_list)
+my_set = set(c)
+my_new = list(c)
+# unique_list = set(c)
+print(my_new)
