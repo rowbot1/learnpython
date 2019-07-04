c97eee0768cec97c143952c9c9cc29f79fdc9c26
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-04 08:50:13 +0100

    Update Divisors.py

--- Divisors.py
+++ Divisors.py
@@ -3,11 +3,10 @@ Exercise 4 (and Solution)
 Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)"""
 
 
-x = (int(input("Enter a Number: ")))
+user_number = (int(input("Enter a Number: ")))
 
-y = []
+list_range = range(1, user_number+1)
 
-for element in x:
-    if n % element == 0:
-        y.append(element)
-       
+for element in list_range:
+    if user_number % element == 0:
+        print(element)
