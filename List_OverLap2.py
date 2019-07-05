56df0eadbf963c37c1d3e9e233b045f208239f1d
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-05 07:37:22 +0100

    Update List_OverLap2.py

--- List_OverLap2.py
+++ List_OverLap2.py
@@ -1,3 +1,11 @@
+"""
+https://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+Take two lists, say for example these two:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes."""
+
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 print("\nNumbers that are both in list a and b: ")
