9392e5997622a2655ccd2f9c1533238daaa13bf0
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-05 15:46:41 +0100

    updated
    updated

--- List_Comprehensions.py
+++ List_Comprehensions.py
@@ -6,5 +6,5 @@ https://youtu.be/3dt4OGnU5sM?t=206"""
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 
 # first 'n' is what i want - always declare this first, 2nd 'n' is part of the for loop.
-even = [n for n in a if n % 2 == 0]
+even = [number for number in a if number % 2 == 0]
 print(even)
---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,29 @@
+"""
+rock beats: scissors
+paper beats: rock
+scissors beat: paper
+
+rock looses to: paper
+paper looses to: rock
+scissors loose to: rock
+
+rock draws with rock
+paper draws with paper
+scissors draws with scissors
+
+"""
+"""idea to make this a network game and randomise who goes first"""
+
+r = "rock"
+p = "paper"
+s = "scissors"
+
+valid = ["r", "p", "s"]
+
+
+# player_1 = input("Player 1 type R, P or S to choose Rock, Paper or Scissors: ")
+# player_2 = input("Player 2 type R, P or S to choose Rock, Paper or Scissors: ")
+
+while True:
+    player_1 = input("Player 1 R P or C: ")
+    if player_1
