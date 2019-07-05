b630b6d71e22d3e86fe8d5d1abd1c5621aadb1b1
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-05 10:50:28 +0100

    Update String_Lists.py

--- String_Lists.py
+++ String_Lists.py
@@ -3,7 +3,7 @@ https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 """
 word = input("Enter Word to test if it is a palindrome: ")
 
-#start at all of the word ':', stop at all of the word ':', step through it one letter at a time backwards '-1'
+#start at all of the word ':', stop at all of the word ':'
 #https://stackoverflow.com/questions/31633635/what-is-the-meaning-of-inta-1-in-python
 # the -1 translates to lent(word)-1
 letter = word[::-1] 
