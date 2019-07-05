69fe8eb1ff69f0030c01c16535580d3b0b4c821a
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-05 22:47:18 +0100

    Update Rock_Paper_Scissors.py

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -10,38 +10,38 @@ scissors loose to: rock
 rock draws with rock
 paper draws with paper
 scissors draws with scissors
+p1	p2
+R	P
+R	S
+P	R
+P	S
+S	R
+S	P
 
 # """
 
-r = "rock"
-p = "paper"
-s = "scissors"
-
-# # ensures the game contines to play after win sequence has completed
-while True:
-    p1 = input("Player 1 R P or C: ").lower()
-    p2 = input("Player 2 R P or C: ").lower()
-    
-    if p1 == p2:
-        print("Draw")
-        
-    elif p1 == r and p2 == p:
-        print("paper wins")
-    elif p1 == r and p2 == s:
-        print("rock wins")
-    elif p1 == p and p2 == r:
-        print("paper wins")
-    elif p1 == p and p2 == s:
-        print("scissors win")
-    elif p1 == s and p2 == r:
-        print("rock wins")
-    elif p1 == s and p2 == p:
-        print("scissors win")
-
-
-
-
-
+rock = "r"
+paper = "p"
+scissors = "s"
+# # ensures the game contines to play after win event has completed
+# while True:
+p1 = input("Player 1 r p or s: ").lower()
+p2 = input("Player 2 r p or s: ").lower()
+# print(p1, p2)
+if p1 == p2:
+    print("Draw")    
+elif p1 == rock and p2 == paper:
+    print(paper, "wins")
+elif p1 == rock and p2 == scissors:   
+    print(rock, " wins")
+elif p1 == paper and p2 == rock:
+    print(scissors," wins")
+elif p1 == paper and p2 == scissors:
+    print(scissors," win")
+elif p1 == scissors and p2 == rock:
+    print(rock," wins")
+elif p1 == scissors and p2 == paper:
+    print(scissors," wins")
 
 
 
