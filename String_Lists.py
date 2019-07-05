236c161123bf3005ab52358632946a0a1848ae71
rowbot1 <34271523+rowbot1@users.noreply.github.com>
2019-07-05 10:40:45 +0100

    Update String_Lists.py

--- String_Lists.py
+++ String_Lists.py
@@ -4,6 +4,7 @@ https://www.practicepython.org/exercise/2014/03/12/06-string-lists.html
 word = input("Enter Word to test if it is a Palindrome: ")
 
 #start at all of the word ':', stop at all of the word ':', step through it one letter at a time backwards '-1'
+#https://stackoverflow.com/questions/31633635/what-is-the-meaning-of-inta-1-in-python
 letter = word[::-1] 
 
 if word == letter:
